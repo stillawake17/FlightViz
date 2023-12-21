3e15f1d949139f7bef28f128a7f9cfac2fa4bdd0
Ioanna Papageorgiou <ioanna2.papageorgiou@uwe.ac.uk>
2023-12-21 23:44:45 +0000

    first update from api

---
+++ src/app.py
@@ -0,0 +1,70 @@
+import requests
+import json
+from datetime import datetime, timedelta
+import os
+
+# Your Aviation Edge API key
+api_key = '97634b-cd2101'
+
+# Base URL for the API
+base_url = "https://aviation-edge.com/v2/public/flightsHistory"
+
+# Function to fetch data for a specific date range and type (arrival/departure)
+def fetch_data(date_from, date_to, type):
+    params = {
+        'key': api_key,
+        'code': 'BRS',
+        'type': type,
+        'date_from': date_from,
+        'date_to': date_to
+    }
+    response = requests.get(base_url, params=params)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error fetching data from {date_from} to {date_to}: {response.status_code}")
+        return None
+
+# Define the path for the JSON file in the data directory
+today_str = datetime.now().strftime("%Y-%m-%d")
+json_file_path = f'data/bristol_airport_{today_str}_data.json'
+
+# Check if the file exists and load existing data
+if os.path.exists(json_file_path):
+    with open(json_file_path, 'r') as file:
+        all_data = json.load(file)
+else:
+    all_data = []
+
+# Define the overall start and end dates for your data retrieval
+overall_start_date = datetime(2023, 1, 1)  # Modify as needed
+overall_end_date = datetime(2023, 12, 31)  # Modify as needed
+
+current_start_date = overall_start_date
+
+# Loop through each 30-day period and fetch data
+while current_start_date <= overall_end_date:
+    current_end_date = min(current_start_date + timedelta(days=30), overall_end_date)
+    start_date_str = current_start_date.strftime("%Y-%m-%d")
+    end_date_str = current_end_date.strftime("%Y-%m-%d")
+    
+    print(f"Fetching data from {start_date_str} to {end_date_str}")
+
+    # Fetch arrival data
+    arrivals = fetch_data(start_date_str, end_date_str, 'arrival')
+    if arrivals:
+        all_data.extend(arrivals)
+
+    # Fetch departure data
+    departures = fetch_data(start_date_str, end_date_str, 'departure')
+    if departures:
+        all_data.extend(departures)
+
+    # Move to the next period
+    current_start_date = current_end_date + timedelta(days=1)
+
+# Save combined data to the JSON file with today's date in the filename
+with open(json_file_path, 'w') as file:
+    json.dump(all_data, file)
+
+print("Data collection complete.")
---
+++ src/monthly_flight_analysis.py
@@ -0,0 +1,24 @@
+import json
+from collections import Counter
+from datetime import datetime
+
+# Define the relative path for the JSON file in the data directory
+# json_file_path = '../data/bristol_airport_data.json'
+json_file_path = 'data\\bristol_airport_2023-12-21_data.json'
+
+# Load the flight data from JSON file
+with open(json_file_path, 'r') as file:
+    data = json.load(file)
+
+# Extract dates from the data
+# Switching to 'actualTime' instead of 'scheduledTime'
+flight_dates = [datetime.strptime(flight['departure']['actualTime'], '%Y-%m-%dT%H:%M:%S.%f').strftime('%Y-%m') 
+                for flight in data 
+                if 'departure' in flight and 'actualTime' in flight['departure']]
+
+# Count flights per month
+flight_counts = Counter(flight_dates)
+
+# Display the flight counts per month
+for month, count in sorted(flight_counts.items()):
+    print(f"Month: {month}, Number of Flights: {count}")

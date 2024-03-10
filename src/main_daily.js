931117ce1678db5652dc454409c2cbd5d8761869
Ioanna Papageorgiou <ioanna2.papageorgiou@uwe.ac.uk>
2024-03-10 00:28:05 +0000

    daily update

--- src/main_daily.js
+++ src/main_daily.js
@@ -71,7 +71,7 @@ function summarizeFlightStatusesWithTimeCategories(flightData) {
 }
 
 // Path to your JSON file
-const filePath = '../EGGD_combined_2024-03-06.json'; // Adjust this path as necessary
+const filePath = '../EGGD_combined_2024-03-08.json'; // Adjust this path as necessary
 
 // Read and parse the JSON file
 fs.readFile(filePath, 'utf8', (err, data) => {
